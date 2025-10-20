b9b63c0e04dd6b773e8ed8047e8dbf1efeae77ee
ryanscordino <hi@ryanscordino.com>
2025-10-20 22:04:33 +0200

    fix: Test to deploy on Cloudflare since Vercel is down

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,16 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
 
+  vite: {
+    resolve: {
+      alias: {
+        // Cloudflare build errors when some packages import node:path default export.
+        // Provide a browser-friendly polyfill so rollup can bundle correctly in Cloudflare.
+        'node:path': 'path-browserify',
+        'path': 'path-browserify'
+      }
+    }
+  },
+
   modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/ui", "@nuxt/image"],
 });
